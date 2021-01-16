19580adb148efbd41af06b7ea939e63e7478a746
Jonathan Hermansen <jhermansen@live.no>
2021-01-16 15:03:05 +0100

    Max/min in array program added

---
+++ e1/max_min_number_in_array.c
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int size, i;
+
+
+int getLargestNumber(int arr[], int size) {
+    int max = arr[0];
+    int i;
+
+    for (i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+int getSmallestNumber(int arr[], int size) {
+    int min = arr[0];
+    int i;
+
+    for (i = 1; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+int main() {
+    size = 10;
+    int arr[size];
+
+    for(i = 0; i < size; i++) {
+        arr[i] = rand() % 101; // random number from 0 to 100
+        printf("%d, ", arr[i]);
+    }
+
+    printf("\nLargest number in array: %d\n", getLargestNumber(arr, size));
+
+    printf("\nSmallest number in array: %d\n", getSmallestNumber(arr, size));
+
+
+}
+
